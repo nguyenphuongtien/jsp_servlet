4f07fbf9042ccea663820bedf46fdfa4cd768c25
Nguyễn Phương Tiền <nphtien.tgg@vnpt.vn>
2020-01-31 16:35:07 +0700

    learn forward servlet

---
+++ SevletTutorial/src/org/o7planning/tutorial/servlet/ServletForward.java
@@ -0,0 +1,32 @@
+package org.o7planning.tutorial.servlet;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+
+@WebServlet("/other/forwardDemo")
+public class ServletForward extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+	
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		String forward = req.getParameter("forward");
+		
+		
+		if("true".equals(forward)) {
+			System.out.println("Forword to show me servlet");
+		}
+	}
+	
+	@Override
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		super.doPost(req, resp);
+	}
+
+}

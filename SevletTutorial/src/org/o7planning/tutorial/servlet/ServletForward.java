e0c924644647c5ea2a37a0258a6f53e004342851
Nguyễn Phương Tiền <nphtien.tgg@vnpt.vn>
2020-01-31 17:19:08 +0700

    ngay mai lam tiep

--- SevletTutorial/src/org/o7planning/tutorial/servlet/ServletForward.java
+++ SevletTutorial/src/org/o7planning/tutorial/servlet/ServletForward.java
@@ -1,32 +1,52 @@
 package org.o7planning.tutorial.servlet;
-
+ 
 import java.io.IOException;
-
+ 
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
+import javax.servlet.ServletOutputStream;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-
-
+ 
+import org.o7planning.tutorial.beans.Constants;
+ 
 @WebServlet("/other/forwardDemo")
 public class ServletForward extends HttpServlet {
-	private static final long serialVersionUID = 1L;
-	
-	@Override
-	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-		String forward = req.getParameter("forward");
-		
-		
-		if("true".equals(forward)) {
-			System.out.println("Forword to show me servlet");
-		}
-	}
-	
-	@Override
-	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		super.doPost(req, resp);
-	}
-
+    private static final long serialVersionUID = 1L;
+ 
+    // Request:
+    // http://localhost:8080/ServletTutorial/other/forwardDemo?forward=true
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+ 
+        // Lấy giá trị tham số (parameter) trên URL.
+        String forward = request.getParameter("forward");
+ 
+        if ("true".equals(forward)) {
+            System.out.println("Forward to ShowMeServlet");
+ 
+            // Sét dữ liệu vào thuộc tính (attribute) của request.
+            request.setAttribute(Constants.ATTRIBUTE_USER_NAME_KEY, //
+                    "Hi, I'm Tom come from Walt Disney !");
+ 
+            RequestDispatcher dispatcher //
+                    = request.getServletContext().getRequestDispatcher("/showMe");
+            dispatcher.forward(request, response);
+ 
+            return;
+        }
+        ServletOutputStream out = response.getOutputStream();
+        out.println("<h3>Text of ForwardDemoServlet</h3>");
+        out.println("- servletPath=" + request.getServletPath());
+    }
+ 
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        this.doGet(request, response);
+    }
+ 
 }
